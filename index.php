dd67ba978231e9034c2c5f4d4f8b2c33b31036bb
pj8912 <jpintofun2b@gmail.com>
2022-02-10 21:26:39 +0530

    test

--- index.php
+++ index.php
@@ -5,13 +5,8 @@
 
 <body>
 
-<div align="center">
+<form method="POST" action="register.php">
 
 
-<form action="" mehtod="post">
-<input type="text" name="mobile" placeholder="Enter Mobile Number">
-<button type="submit" name="verify" > Verify </button>
 
 </form>
-
-</div>
